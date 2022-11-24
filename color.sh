b9a2958bf5a97d6873aedc044704316019980701
Joe Irvine <jirvine124@gmail.com>
2022-11-24 02:20:28 -0700

    added control structure outline and useage.

--- color.sh
+++ color.sh
@@ -1,28 +1,73 @@
 #!/bin/bash 
 #
-# TODO - Add a usage function 
+usage() {
+    echo "$0 [OPTION] Value"
+    echo 
+    echo "Options:"
+    echo 
+    echo " -c"
+    echo "convert Hexidemical to RGB"
+    echo 
+    echo " -r"
+    echo "convert RGB to Hexidemcial"
+}
+# TODO - Decide how to handle conversion.
+# TODO - Create Functions to handle converions.
+
+
 # If no argument is given 
 if [[ ${#} -eq 0 ]]
 then
     echo "Please Supply an arugment" 
-    # Add a usage function 
+    usage
     exit 1
 fi
-COLOR=${1} 
+# If check to see how many arguments are given and assign the color variable 
+if [[ ${#} -eq 1 ]]
+then 
+    COLOR=${1}
+else
+    COLOR=${2}
+fi
 
-# IF Hexadeicmal color code is provide 
-# Break up number in pairs of 2 
-RED=${COLOR:1:2}
-BLUE=${COLOR:3:2}
-GREEN=${COLOR:5:2}
+## Get Options 
+while [ "$#" -gt 0 ]
+do 
+    case "$1" in 
+        -c) HEX=TRUE ;;
+        -w) RGB=TRUE ;;
+        -*) echo "Going to attempt automatic conversion" 
+            ;;
+    esac
+    shift
+done
 
-# Convert From Base 16
-BLUE=$(bc <<< "ibase=16; $BLUE")
-RED=$(bc <<< "ibase=16; $RED")
-GREEN=$(bc <<< "ibase=16; $GREEN")
+if [[ ${HEX} ]] 
+then
+    RED=${COLOR:1:2}
+    BLUE=${COLOR:3:2}
+    GREEN=${COLOR:5:2}
 
-# Print Results
-printf "\x1b[38;2;${RED};${GREEN};${BLUE}mrgb(${RED}, ${GREEN}, ${BLUE})\x1b[0m\n"
-printf "\x1b[48;2;${RED};${GREEN};${BLUE}mrgb(${RED}, ${GREEN}, ${BLUE})\x1b[0m\n"
+    # Convert From Base 16
+    BLUE=$(bc <<< "ibase=16; $BLUE")
+    RED=$(bc <<< "ibase=16; $RED")
+    GREEN=$(bc <<< "ibase=16; $GREEN")
+
+    # Print Results
+    printf "\x1b[38;2;${RED};${GREEN};${BLUE}mrgb(${RED}, ${GREEN}, ${BLUE})\x1b[0m\n"
+    printf "\x1b[48;2;${RED};${GREEN};${BLUE}mrgb(${RED}, ${GREEN}, ${BLUE})\x1b[0m\n"
+
+
+elif [[ $RGB ]]
+then
+    echo "I'll convert from RGB"
+else 
+    if [[ "${COLOR:0:1}" = "#" ]]
+    then 
+        echo "I'm going to convert this here hexidemical"
+    else 
+        echo "I'm going to convert this RGB value"
+    fi
+fi
 
 
