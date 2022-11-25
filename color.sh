2cd3212b30477275d028e3e618ed1a7da3f6dc40
Joe Irvine <jirvine124@gmail.com>
2022-11-24 23:35:30 -0700

    added rgb to hex conversion

--- color.sh
+++ color.sh
@@ -2,45 +2,30 @@
 #
 usage() {
     echo "$0 [OPTION] Value"
-    echo 
-    echo "Options:"
-    echo 
-    echo " -c"
-    echo "convert Hexidemical to RGB"
-    echo 
-    echo " -r"
-    echo "convert RGB to Hexidemcial"
 }
-# TODO - Decide how to handle conversion.
-# TODO - Create Functions to handle converions.
 
-
-# If no argument is given 
 if [[ ${#} -eq 0 ]]
 then
     echo "Please Supply an arugment" 
     usage
     exit 1
 fi
-# If check to see how many arguments are given and assign the color variable 
-if [[ ${#} -eq 1 ]]
-then 
-    COLOR=${1}
-else
-    COLOR=${2}
-fi
 
-## Get Options 
-while [ "$#" -gt 0 ]
+# Get Options
+# TODO Add Better options 
+while [ "$#" -gt 1 ]
 do 
     case "$1" in 
-        -c) HEX=TRUE ;;
+        -c) HEX=TRUE
+            echo "This -c option was selected";;
         -w) RGB=TRUE ;;
         -*) echo "Going to attempt automatic conversion" 
             ;;
     esac
     shift
 done
+
+COLOR=${@}
 
 if [[ ${HEX} ]] 
 then
@@ -64,9 +49,57 @@ then
 else 
     if [[ "${COLOR:0:1}" = "#" ]]
     then 
-        echo "I'm going to convert this here hexidemical"
+        RED=${COLOR:1:2}
+        BLUE=${COLOR:3:2}
+        GREEN=${COLOR:5:2}
+
+        # Convert From Base 16
+        BLUE=$(bc <<< "ibase=16; $BLUE")
+        RED=$(bc <<< "ibase=16; $RED")
+        GREEN=$(bc <<< "ibase=16; $GREEN")
+
+        # Print Results
+        printf "\x1b[38;2;${RED};${GREEN};${BLUE}mrgb(${RED}, ${GREEN}, ${BLUE})\x1b[0m\n"
+        printf "\x1b[48;2;${RED};${GREEN};${BLUE}mrgb(${RED}, ${GREEN}, ${BLUE})\x1b[0m\n"
+
     else 
-        echo "I'm going to convert this RGB value"
+        # Will need to iterate through string from comma to comma 
+        LENGTH=${#COLOR}
+        RED=""
+        GREEN=""
+        BLUE="" 
+        for (( c=0; c<=$LENGTH; c++ )) 
+        do 
+            if [[ ${COLOR:$c:1} = "," ]] || [[ $c = $LENGTH ]]
+            then 
+              #Change Color Variable  
+              if [[ ${#RED} -eq 0 ]]
+              then 
+                  RED=$CURRENT 
+                  CURRENT="" 
+              elif [[ ${#GREEN} -eq 0 ]]
+              then 
+                  GREEN=$CURRENT
+                  CURRENT=""
+              elif [[ ${#BLUE} -eq 0 ]] 
+              then
+                  BLUE=$CURRENT 
+                  CURRENT=""
+              else 
+                  echo "There was an error, I dunno"
+              fi
+            else 
+                CURRENT=$CURRENT${COLOR:$c:1}
+            fi
+        done
+        # Convert base 10 to base 16
+        HBLUE=$(bc <<< "ibase=10; obase=16; $BLUE")
+        HRED=$(bc <<< "ibase=10; obase=16; $RED")
+        HGREEN=$(bc <<< "ibase=10; obase=16; $GREEN")
+        printf "\x1b[38;2;${RED};${GREEN};${BLUE}m#${HRED}${HGREEN}${HBLUE}\x1b[0m\n"
+        printf "\x1b[48;2;${RED};${GREEN};${BLUE}m#${HRED}${HGREEN}${HBLUE}\x1b[0m\n"
+
+
     fi
 fi
 
